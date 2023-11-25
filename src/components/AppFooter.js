5f48e4cc8c2aefc5f2afdeb8bc6dd79ae3a0e84d
Nicolas Graf <nicolas.o.graf@gmail.com>
2023-11-25 22:45:43 +0100

    footer responsive and remove hearticon

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,7 +5,7 @@ import { AiFillHeart } from "react-icons/ai";
 const AppFooter = () => {
   return (
     <Footer className="shadow-[0px_-3px_6px_0px_#00000018]" container>
-      <div className="w-full sm:flex sm:items-center sm:justify-between dark:text-secondary-light">
+      <div className="w-full gap-2 flex flex-col md:flex-row items-center md:justify-between dark:text-secondary-light">
         <Footer.Copyright href="#" by="NOG" year={2023} />
         <div className="flex justify-center gap-1.5 items-center text-md">
           <span>Made with</span>
@@ -16,7 +16,7 @@ const AppFooter = () => {
             by nog.codes
           </a>
         </div>
-        <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+        <div className="mt-4 flex space-x-6 md:mt-0 sm:justify-center">
           <Footer.Icon href="#" target="_blank" icon={BsInstagram} />
           <Footer.Icon href="#" icon={BsTwitter} />
           <Footer.Icon href="#" icon={BsGithub} />
